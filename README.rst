16fe770940d834202a0bea86990c025f406e2827
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-06-22 18:09:53 -0400

    Update to 1.2.8

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 ##############
-pfioh - v1.2.7
+pfioh - v1.2.8
 ##############
 
 .. image:: https://badge.fury.io/py/pman.svg
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
       name             =   'pfioh',
-      version          =   '1.2.7',
+      version          =   '1.2.8',
       description      =   'Path-and-File-IO-over-HTTP',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
