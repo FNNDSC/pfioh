186b7ad61100c9656cc6daefb8793a6eb97684ab
Rudolph Pienaar <rudolph.pienaar@gmail.com>
2017-09-15 17:08:57 -0400

    Update some incorrect calls and note some possible logic errors.

--- README.rst
+++ README.rst
@@ -1,5 +1,5 @@
 ##############
-pfioh - v1.4.5
+pfioh - v1.4.8
 ##############
 
 .. image:: https://badge.fury.io/py/pman.svg
--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ def readme():
 
 setup(
       name             =   'pfioh',
-      version          =   '1.4.5',
+      version          =   '1.4.8',
       description      =   'Path-and-File-IO-over-HTTP',
       long_description =   readme(),
       author           =   'Rudolph Pienaar',
       author_email     =   'rudolph.pienaar@gmail.com',
       url              =   'https://github.com/FNNDSC/pman',
       packages         =   ['pfioh'],
-      install_requires =   ['pycurl', 'pyzmq', 'webob', 'pudb', 'psutil'],
+      install_requires =   ['pycurl', 'pyzmq', 'webob', 'pudb', 'psutil', 'keystoneauth1', 'python-swiftclient'],
       test_suite       =   'nose.collector',
       tests_require    =   ['nose'],
       scripts          =   ['bin/pfioh'],
